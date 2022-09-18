7c1b1d46d1977de6bf4d6f62424b7fb7ad7ad735
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-09-18 14:47:12 +0530

    Update GenericMaxNum.java

--- GenericMaxNum.java
+++ GenericMaxNum.java
@@ -10,15 +10,16 @@ public class GenericMaxNum
 		// TODO Auto-generated method stub
 
 		Integer num1, num2, num3;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter 3 values : ");
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        num3 = sc.nextInt();
-        solve(num1, num2, num3);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 3 values : ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		solve(num1, num2, num3);
 	}
 
-	private static void solve(Integer num1, Integer num2, Integer num3) {
+	private static void solve(Integer num1, Integer num2, Integer num3) 
+	{
 		// TODO Auto-generated method stub
 		
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0 ) 
@@ -28,7 +29,6 @@ public class GenericMaxNum
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) 
 		{
 			System.out.println(num2 + " is greatest");
-
 		}
 		else 
 		{
